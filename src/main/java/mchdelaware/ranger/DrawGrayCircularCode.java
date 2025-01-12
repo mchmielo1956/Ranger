a1e6125beb25f308b7d6bfb27afcafb5ca084464
marek <marek@mnich>
2025-01-12 19:52:35 +0100

    Fix names

--- src/main/java/mchdelaware/ranger/DrawGrayCircularCode.java
+++ src/main/java/mchdelaware/ranger/DrawGrayCircularCode.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package mchdelaware.rander;
+package mchdelaware.ranger;
 
 import org.slf4j.LoggerFactory;
 import java.awt.*;
--- src/main/java/mchdelaware/ranger/DrawingPanel.java
+++ src/main/java/mchdelaware/ranger/DrawingPanel.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package mchdelaware.rander;
+package mchdelaware.ranger;
 
 import javax.swing.JPanel;
 import java.awt.Graphics;
